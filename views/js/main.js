8a127d5ecf4d9f49cffe390eb9185170e5f6fe56
Jayantth <jayanth.ganesh07@gmail.com>
2016-10-22 20:17:43 +0530

    docs: Time to resize pizzas is less than 5 ms using the pizza size slider on the views/pizza.html page. Resize time is shown in the browser developer tools.
    This part is broken due to a bug in the resizing code. Corrections are made so that Time to resize pizzas is less than 5 ms using the pizza size slider on the views/pizza.html page.

--- views/js/main.js
+++ views/js/main.js
@@ -427,8 +427,10 @@ var resizePizzas = function(size) {
       switch(size) {
         case "1":
           newWidth = 25;
+          break;
         case "2":
           newWidth = 33.33;
+          break;
         case "3":
           newWidth = 50;
           break;
